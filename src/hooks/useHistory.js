f076c0e8b9edad19d43cb7b61cd908af86e85381
phongnguyen <phongnguyen25000@gmail.com>
2025-12-04 13:56:21 +0100

    implement max history

--- src/hooks/useHistory.js
+++ src/hooks/useHistory.js
@@ -1,31 +1,33 @@
-import {useState, useEffect, useContext} from "react";
-import {SongContext} from "../contexts/SongContext";
+import { useState, useEffect, useContext } from "react";
+import { SongContext } from "../contexts/SongContext";
 
 export function useHistory() {
-    const {songTitle} = useContext(SongContext);
-    const [history, setHistory] = useState([]);
+  const { songTitle } = useContext(SongContext);
+  const [history, setHistory] = useState([]);
 
-    useEffect(() => {
-        const storedHistory = JSON.parse(
-            localStorage.getItem("songHistory") || "[]"
-        );
-        setHistory(storedHistory);
-    }, []);
+  useEffect(() => {
+    const storedHistory = JSON.parse(
+      localStorage.getItem("songHistory") || "[]"
+    );
+    setHistory(storedHistory);
+  }, []);
 
-    useEffect(() => {
-        if (songTitle) {
-            setHistory((prevHistory) => {
-                const lastSong = prevHistory[0];
-                if (lastSong !== songTitle) {
-                    const updatedHistory = [songTitle, ...prevHistory];
-                    localStorage.setItem("songHistory", JSON.stringify(updatedHistory));
-                    return updatedHistory;
-                }
-                return prevHistory;
-
-            });
+  useEffect(() => {
+    if (songTitle) {
+      setHistory((prevHistory) => {
+        const lastSong = prevHistory[0];
+        if (lastSong !== songTitle) {
+          let updatedHistory = [songTitle, ...prevHistory];
+          if (updatedHistory.length > 14) {
+            updatedHistory = updatedHistory.slice(0, 14);
+          }
+          localStorage.setItem("songHistory", JSON.stringify(updatedHistory));
+          return updatedHistory;
         }
-    }, [songTitle]);
+        return prevHistory;
+      });
+    }
+  }, [songTitle]);
 
-    return history;
+  return history;
 }
