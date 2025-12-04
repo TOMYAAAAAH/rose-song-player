0695049c3da63b04fc9d29155bd827ba924933c0
Colleu Arthur <colleuarthur@gmail.com>
2025-12-04 14:48:26 +0100

    Add icon 2

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -13,10 +13,7 @@ export default function Aside() {
     <div className="bg-gray-300 w-64 p-5 top-0 transition gap-3 flex flex-col overflow-y-hidden">
       <div>
         <p>Historique</p>
-        <TrashIcon
-          onClick={play}
-          className="w-12 h-12 text-black cursor-pointer"
-        />
+        <TrashIcon className="w-12 h-12 text-black cursor-pointer" />
       </div>
       {history.map((songTitle, index) => {
         const relatedTrack = tracks.find((track) => songTitle === track.title);
